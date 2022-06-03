27d4f25ec5762e913e86f36b5ee98a7e113a0735
Timothy Axford <t.axford2@yahoo.com>
2022-06-02 21:45:29 -0400

    Add project files.

---
+++ HelloWorld2.0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2
+    {
+        static void Main(string[] args)
+        {
+        //Greeting
+        string greetingOne = "Hello World!'";
+        string greetingTwo = "My name is Timothy Axford II";
+    }
