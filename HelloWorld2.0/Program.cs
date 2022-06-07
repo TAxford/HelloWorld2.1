a1d87a3509d31ea13856b6d95603839293d62148
Timothy Axford <t.axford2@yahoo.com>
2022-06-06 22:25:45 -0400

    Added wifes name as a string, introduced her and conducted a mathmatical subtraction of our ages

--- HelloWorld2.0/Program.cs
+++ HelloWorld2.0/Program.cs
@@ -11,22 +11,31 @@ namespace HelloWorld2
         static void Main(string[] args)
         {
             //Greeting
-            string greetingOne = "Hello World!'";
+            string greetingOne = "Hello World!";
             string greetingTwo = "My name is Timothy Axford II";
 
 
             //Give myself Attributes
+            string wifeName = "Rebecca";
             int myAge = 36;
+            int wifeAge = 34;
             double myHeight = 5.11;
             bool isMarried = true;
             bool hasChildren = true;
             bool isChild = false;
             char myGender = 'M';
 
+            //introducing myself
             Console.WriteLine(greetingOne + greetingTwo);
             Console.WriteLine("I am " + myAge + " and my marriage status is " + isMarried);
-            Console.WriteLine("Am I a child? " + isChild + " however, do I have children? This is " + hasChildren); 
+            Console.WriteLine("Am I a child? " + isChild + " however, do I have children? This is " + hasChildren);
 
+            //Mathmathmatic introduction
+            Console.WriteLine("The diffrence in age between my wife and I is ");
+            Console.WriteLine(myAge - wifeAge + " years");
+
+            //Introduction of wife
+            Console.WriteLine("My wifes name is " + wifeName);
 
 
 
